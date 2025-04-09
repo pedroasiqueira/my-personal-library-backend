fea1f515e71272c78e654c7f75c9d1da5a8e9504
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2025-04-09 01:46:01 -0300

    Salvando alterações

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://db_user:<q1hrbTmpuMn64nFB>@cluster0.1jxpg5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot('mongodb+srv://db_user:<bA6hHwruQiQLEh3b>@cluster0.1jxpg5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
     UsersModule,
   ],
   controllers: [AppController],
