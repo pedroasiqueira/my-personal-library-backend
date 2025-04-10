ccfecbf2962e0f768610f6ff1d7d3fa7dc1e79c2
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2025-04-09 23:37:31 -0300

    Implementação do módulo de livros com controller, service, DTOs e testes. Adiciona script pgit.sh para facilitar commits e pushs.
     /2025-04-09--23:37:31

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://db_user:bA6hHwruQiQLEh3b@cluster0.1jxpg5v.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'),
     UsersModule,
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
