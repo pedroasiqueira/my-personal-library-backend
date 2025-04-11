61803c96a5e74288aec83b43b9a967ba48d7f06d
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2025-04-11 15:18:38 -0300

    Atualiza a configuração de CORS para permitir acesso ao novo domínio do frontend no Vercel.
     /2025-04-11--15:18:38

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   
   // Configuração do CORS
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://my-personal-library-frontend.vercel.app'],
+    origin: ['http://localhost:3001', 'https://meuhubdelivros.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
