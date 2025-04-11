e394d0025be41130dd02e3eeec545a3295920537
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2025-04-11 17:54:35 -0300

    testando se vai funcionar

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,10 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
-    ConfigModule.forRoot(), // isso permite ler o .env automaticamente
-    MongooseModule.forRoot(process.env.MONGO_URI ?? ''),
+    MongooseModule.forRoot('mongodb+srv://db_user:bA6hHwruQiQLEh3b@cluster0.1jxpg5v.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'),
     UsersModule,
     BooksModule,
     AuthModule,
