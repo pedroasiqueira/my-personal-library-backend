096e1d3ced1979ee96d28ac0bc815427c908cff2
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2025-04-11 21:54:31 -0300

    TESTE5
     /2025-04-11--21:54:31

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot('mongodb+srv://db_user:bA6hHwruQiQLEh3b@cluster0.1jxpg5v.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? ''),
     UsersModule,
     BooksModule,
     AuthModule,
